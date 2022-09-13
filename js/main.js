f57514ac8fe9c702fd8050db0b096b38694dc387
Dmither <mr.d.graumann@gmail.com>
2022-09-13 17:12:40 +0300

    Change description

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 $(function(){
-    function changeAttribute(element, attribute, action={ADD: 'add', CHANGE: 'change', REMOVE: 'remove'}, value=''){
-        if (action=ADD){
-            element.attr(attribute);
-        } else if (action=CHANGE){
-            element.attr(attribute, value);
-        } else {
-            element.removeAttr(attribute);
-        }
-    }
-    changeAttribute($('.logo img', 'src', ADD, img/logo.png))
+    $(':submit').click(function(){
+        let value = $('textarea').val();
+        alert(value);
+    })
 })
